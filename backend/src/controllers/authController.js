3b9f4ac014efcaaa09cf6f94738b59f4f6abbc26
OM <omjaiswal2552@gmail.com>
2026-03-17 17:26:24 +0530

    updated authController

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -13,8 +13,8 @@ function sendTokenCookie(res, token) {
   res.cookie("token", token, {
     httpOnly: true,                                // JS can't access it
     secure: process.env.NODE_ENV === "production", // HTTPS only in production
-    // sameSite:( (process.env.NODE_ENV === 'production') ? "None": "Lax"),
-    sameSite: "None",
+    sameSite:( (process.env.NODE_ENV === 'production') ? "None": "Lax"),
+    // sameSite: "None",
     maxAge: 7 * 24 * 60 * 60 * 1000,             // 7 days in ms
   });
 }
